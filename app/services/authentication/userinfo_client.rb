91af05cf487e76903edb1c7692d985cfd14d6014
torihazi <lovebarusa19981225@icloud.com>
2025-12-06 15:14:34 +0900

    rm user info client

--- app/services/authentication/userinfo_client.rb
+++ app/services/authentication/userinfo_client.rb
@@ -1,21 +1,23 @@
-# このクラスがやることは google の userinfoエンドポイントにリクエストしてユーザ情報を取得する
-# それを行う上で何が必要だろうか。
-# endpointは 固定
-# responseの形式はjsonである
+# # このクラスがやることは google の userinfoエンドポイントにリクエストしてユーザ情報を取得する
+# # それを行う上で何が必要だろうか。
+# # endpointは 固定
+# # responseの形式はjsonである
+# # endpointにリクエストして、responseを取得してそれをsetupに返す？
+# # いらないかも setuoに含めてしまって良い。
 
-module Authentication
-  class UserinfoClient
-    ENDPOINT = 'https://www.googleapis.com/oauth2/v3/userinfo'
+# module Authentication
+#   class UserinfoClient
+#     ENDPOINT = 'https://www.googleapis.com/oauth2/v3/userinfo'
 
-    def self.fetch(token)
-      new(token).fetch
-    end
+#     def self.fetch(token)
+#       new(token).fetch
+#     end
 
-    def fetch
-      response = Infra::Http::Client.get(ENDPOINT, headers: { 'Authorization' => "Bearer #{token}" })
-      # ここでresponseをパースしてjsonを返さないといけない
-      # ここでjson parseするのは違う気がする。
-      # json parseするのはもっと下のレイヤーで行うべき
-    end
-  end
-end
+#     def fetch
+#       response = Infra::Http::Client.get(ENDPOINT, headers: { 'Authorization' => "Bearer #{token}" })
+#       # ここでresponseをパースしてjsonを返さないといけない
+#       # ここでjson parseするのは違う気がする。
+#       # json parseするのはもっと下のレイヤーで行うべき
+#     end
+#   end
+# end
